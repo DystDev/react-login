970e3d7d188a80027251de7467a8c4989ef7da81
DystDev <58528580+DystDev@users.noreply.github.com>
2022-02-25 21:41:11 +0000

    Danny P code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-/*jshint esversion: 6 */
-
-/* jshint ignore:start */
-
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Mogus from './Info.js'
+
 
 export default class App extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = { name: null, authorized: false };
-            this.authorize = this.authorize.bind(this);
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      password: 'amogos',
+      authorized: false
+    }
+    this.authorize = this.authorize.bind(this)
+  }
 
-        authorize(e) {
 
-        }
+  authorize() {
+    this.setState({authorized: true});
+  }
 
-        render() {
-            return ( < h1 > { this.state.authorized ? 'Authorized' : 'Not authorized' } < /h1> )
-            }
-        }
+
+  render() {
+    const button = (<button onClick={this.authorize}>Click</button>)
+    return (
+      <div>
+        <h1>State is: {this.state.authorized ? 'YES' : 'NO LLLLLLLLLL'}</h1>
+        {this.state.authorized ? <Mogus /> : button}
+      </div>
+      
+    )
+  }
+}
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ src/Info.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DannyP from "./dannyp.png"
+
+
+export default class Mogus extends React.Component {
+    render() {
+        return (
+            <img src={DannyP} alt="lol"></img>
+        )
+    }
+}
