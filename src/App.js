ad3fddb517160500329a30e0d4e268f6ed6960ff
DystDev <58528580+DystDev@users.noreply.github.com>
2022-02-25 11:17:01 +0000

    Authorize Text
    - No func yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/*jshint esversion: 6 */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* jshint ignore:start */
 
-export default App;
+import React from 'react';
+
+export default class App extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = { name: null, authorized: false };
+            this.authorize = this.authorize.bind(this);
+        }
+
+        authorize(e) {
+
+        }
+
+        render() {
+            return ( < h1 > { this.state.authorized ? 'Authorized' : 'Not authorized' } < /h1> )
+            }
+        }
